f213c0f10d874885aca1ccfeb93c75a050f1829f
Adith-900 <adithmanojan900@gmail.com>
2024-10-23 21:38:34 +0530

    commit first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ErrorComponent } from './pages/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'', component: HomeComponent
+    },
+    {
+        path:'product', component: ProductComponent
+    },
+    {
+        path:'**', component: ErrorComponent
+    },
+    
+];
